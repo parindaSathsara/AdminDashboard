783836fb7107e93dfdf0de29eec085fc8c6897f8
viduah <developer5@appletechlabs.com>
2024-04-09 18:09:45 +0530

    CHG | Chat UI

--- src/views/_Chats/ChatsIndex.js
+++ src/views/_Chats/ChatsIndex.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './ChatsIndex.css';
 import UserList from './Components/UserList';
 import MessageList from './Components/MessageList';
-import {CRow,CCol} from '@coreui/react';
+import { CRow, CCol } from '@coreui/react';
 
 const ChatsIndex = () => {
   return (
     <div className='chat-index'>
       <CRow>
-        <CCol xs={3}>
+        <CCol xs={4}>
           <UserList></UserList>
         </CCol>
-        <CCol xs={9}>
-        <MessageList></MessageList>
+        <CCol xs={8}>
+          <MessageList></MessageList>
         </CCol>
       </CRow>
     </div>
