df37081b29a408e74ca1e1da005b92a94d2842f7
viduah <developer5@appletechlabs.com>
2024-04-11 08:54:50 +0530

    CHG | Message text update

--- src/views/_Chats/Components/Message.js
+++ src/views/_Chats/Components/Message.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { CAvatar, CCol, CRow } from '@coreui/react';
 import './Message.css'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Message = (props) => {
   return (
@@ -13,6 +15,8 @@ const Message = (props) => {
           <div className='message-content'>
             {props.messageData.text}
           </div>
+          <FontAwesomeIcon className='fa-icons' icon={faEdit} />
+          <FontAwesomeIcon className='fa-icons' icon={faTrash} />
         </CCol>
       </CRow>
     </div >
