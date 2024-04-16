b2ba939ed01aee6190aecfe001d59d4c34fb55ec
viduah <developer5@appletechlabs.com>
2024-04-16 16:41:48 +0530

    CHG | Send message

--- src/views/_Chats/Components/MessageList.js
+++ src/views/_Chats/Components/MessageList.js
@@ -10,6 +10,7 @@ import Message from './Message';
 import { event } from 'jquery';
 import { db } from 'src/firebase';
 import { addDoc, collection, getDocs, limit, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 const textColor = {
   color: 'white'
@@ -18,6 +19,8 @@ const textColor = {
 const MessageList = (props) => {
 
   const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState('');
+  const [collection_id, setCollection_id] = useState(null);
 
   const messageContailerRef = useRef(null);
 
@@ -30,6 +33,7 @@ const MessageList = (props) => {
   useEffect(() => {
     if (props.conversation_data) {
       fetchMessages(props.conversation_data.customer_collection_id);
+      setCollection_id(props.conversation_data.customer_collection_id);
     }
   }, [props.conversation_data]);
 
@@ -38,14 +42,31 @@ const MessageList = (props) => {
     scrollDown();
   }, [messageList]);
 
-  const [message, setMessage] = useState('');
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   }
 
-  const sendMessage = () => {
+  const sendMessage = async () => {
+    if (message.trim() == "") {
+      return
+    }
+    await addDoc(collection(db, "chats/chats_dats/" + collection_id), {
+      text: message,
+      name: "Admin",
+      avatar: '',
+      createdAt: serverTimestamp(),
+      role: "Admin",
+      readAt: null,
+      uid: "admin"
+    })
     setMessage('')
+  }
+
+  const sendMessageByEnter = (event) => {
+    if (event.keyCode === 13) {
+      sendMessage();
+    }
   }
 
   const fetchMessages = (id) => {
@@ -63,7 +84,13 @@ const MessageList = (props) => {
     <div className='message-list'>
       <CRow className='message-list-header-bar p-3'>
         <CCol sm={1}>
-          <CAvatar color="primary" textColor="white" size="m">UM</CAvatar>
+          {props.conversation_data && (props.conversation_data.chat_avatar ? (
+            <CContainer className='avatar-cover'>
+              <img className='chat-avatar' src={props.conversation_data.chat_avatar} alt='Avatar' />
+            </CContainer>
+          ) : (
+            <CAvatar color="primary" textColor="white" size='lg'>{props.conversation_data.chat_name ? (props.conversation_data.chat_name.toUpperCase().slice(0, 2)) : ''}</CAvatar>
+          ))}
         </CCol>
         <CCol style={textColor} className='d-flex align-items-center' sm={9}>
           {props.conversation_data && props.conversation_data.chat_name}
@@ -83,7 +110,7 @@ const MessageList = (props) => {
 
       <CRow className='p-2' style={textColor}>
         <CCol sm={11}>
-          <CFormInput type="text" placeholder="Type a message" onChange={handleMessageChange} value={message} />
+          <CFormInput type="text" placeholder="Type a message" onChange={handleMessageChange} value={message} onKeyDown={(event) => sendMessageByEnter(event)} />
         </CCol>
         <CCol sm={1} style={{ padding: 0 }}>
           <CButton color="light"><FontAwesomeIcon icon={faPaperPlane} onClick={sendMessage} /></CButton>
