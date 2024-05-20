cf33679e8e6f2710332d78eea8e46589e05b4060
parindaSathsara <parindasathsara2014@gmail.com>
2024-05-20 23:07:06 +0530

    commit changes

--- src/service/InAppNotificationService.jsx
+++ src/service/InAppNotificationService.jsx
@@ -28,6 +28,8 @@ export default function InAppNotificationService() {
         );
     };
 
+
+    //
     const addNewToast = () => {
         setToasts([...toasts, exampleToast()]);  // Add new toast to the array
     };
