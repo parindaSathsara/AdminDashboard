5d084255cebf7005f52ded210bf43b6dd232c983
viduah <developer5@appletechlabs.com>
2024-11-07 09:42:55 +0530

    Supplier voucher downnload fix on emails

--- src/views/EmailDashboard/services/emailServices.jsx
+++ src/views/EmailDashboard/services/emailServices.jsx
@@ -3,16 +3,16 @@ import Swal from "sweetalert2";
 
 const getOrderIDs = async () => {
 
-    var dataSet = [];
+  var dataSet = [];
 
-    await axios.get("get_order_ids").then(response => {
-        if (response.data.status === 200) {
-            dataSet = response.data.orderIds
-        }
-    })
+  await axios.get("get_order_ids").then(response => {
+    if (response.data.status === 200) {
+      dataSet = response.data.orderIds
+    }
+  })
 
 
-    return dataSet
+  return dataSet
 }
 
 
@@ -20,147 +20,147 @@ const getOrderIDs = async () => {
 const downloadOrderReceipt = async (id) => {
 
 
-    try {
-        // Constructing the URL for the PDF download
-        const url = `https://gateway.aahaas.com/api/downloadOrderReceipt/${id}`;
+  try {
+    // Constructing the URL for the PDF download
+    const url = `https://gateway.aahaas.com/api/downloadOrderReceipt/${id}`;
 
-        // Open the URL in a new tab/window
-        window.open(url, '_blank');
+    // Open the URL in a new tab/window
+    window.open(url, '_blank');
 
-    } catch (error) {
-        console.error('Error fetching PDF:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error('Error fetching PDF:', error);
+    throw error;
+  }
 
 
 }
 
 
 const downloadSupplierVoucherOneByOne = async (id, goid) => {
-    try {
-        // Constructing the URL for the PDF download
-        const url = `https://gateway.aahaas.com/api/downloadIndividualPDF/${id}/${goid}`;
+  try {
+    // Constructing the URL for the PDF download
+    const url = `${axios.defaults.baseURL}/supplier-voucher/${id}/pdf`;
 
-        // Open the URL in a new tab/window
-        window.open(url, '_blank');
+    // Open the URL in a new tab/window
+    window.location.href = url
 
-    } catch (error) {
-        console.error('Error fetching PDF:', error);
-        throw error;
-    }
+  } catch (error) {
+    console.error('Error fetching PDF:', error);
+    throw error;
+  }
 
 }
 
 const downloadAllSupplierVouchers = async (id, groupIds = []) => {
 
-    groupIds.map(res => {
+  groupIds.map(res => {
 
-        // console.log(res, "Group ID data values are")
-        downloadSupplierVoucherOneByOne(res, id)
-    })
+    // console.log(res, "Group ID data values are")
+    downloadSupplierVoucherOneByOne(res, id)
+  })
 }
 
 
 
 const resendOrderEmailToCustomer = (id) => {
-    // console.log(id, "ID Value is");
+  // console.log(id, "ID Value is");
 
-    Swal.fire({
-        title: 'Sending...',
-        text: 'Resending the order email, please wait.',
-        allowOutsideClick: false,
-        onBeforeOpen: () => {
-            Swal.showLoading();
-        }
+  Swal.fire({
+    title: 'Sending...',
+    text: 'Resending the order email, please wait.',
+    allowOutsideClick: false,
+    onBeforeOpen: () => {
+      Swal.showLoading();
+    }
+  });
+
+  axios.post(`https://gateway.aahaas.com/api/receipt/create/mail/${id}`)
+    .then(res => {
+      if (res.data.status === 200) {
+        Swal.fire(
+          'Email Sent!',
+          'The order email has been resent successfully.',
+          'success'
+        );
+      } else {
+        Swal.fire(
+          'Error!',
+          'There was an issue resending the order email.',
+          'error'
+        );
+      }
+    })
+    .catch(error => {
+      Swal.fire(
+        'Error!',
+        'There was an issue resending the order email.',
+        'error'
+      );
     });
-
-    axios.post(`https://gateway.aahaas.com/api/receipt/create/mail/${id}`)
-        .then(res => {
-            if (res.data.status === 200) {
-                Swal.fire(
-                    'Email Sent!',
-                    'The order email has been resent successfully.',
-                    'success'
-                );
-            } else {
-                Swal.fire(
-                    'Error!',
-                    'There was an issue resending the order email.',
-                    'error'
-                );
-            }
-        })
-        .catch(error => {
-            Swal.fire(
-                'Error!',
-                'There was an issue resending the order email.',
-                'error'
-            );
-        });
 };
 
 const confirmResendEmail = (id) => {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "Do you want to resend the order email?",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, resend it!',
-        cancelButtonText: 'No, cancel!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            resendOrderEmailToCustomer(id);
-        }
-    });
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "Do you want to resend the order email?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, resend it!',
+    cancelButtonText: 'No, cancel!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      resendOrderEmailToCustomer(id);
+    }
+  });
 };
 
 
 
 const resendAllSupplierVouchers = (id, indexId, goid) => {
-    if (id == "All") {
+  if (id == "All") {
 
-        axios.post(`https://gateway.aahaas.com/api/receipt-items/create/mail/${id}`).then(res => {
+    axios.post(`https://gateway.aahaas.com/api/receipt-items/create/mail/${id}`).then(res => {
 
-        })
+    })
 
-    }
-    else {
-        axios.post(`https://gateway.aahaas.com/api/sendOrderIndividualItemMailsVoucher/${indexId}/${goid}/A`).then(res => {
+  }
+  else {
+    axios.post(`https://gateway.aahaas.com/api/sendOrderIndividualItemMailsVoucher/${indexId}/${goid}/A`).then(res => {
 
-        })
-    }
+    })
+  }
 }
 
 
 
 
 const fetchAllCustomerWithEmails = async () => {
-    var customerEmails = [];
+  var customerEmails = [];
 
-    await axios.get(`get_customer_with_emails/${id}`).then(response => {
-        if (response.data.status == 200) {
-            customerEmails = response.data.customers
-        }
-    })
+  await axios.get(`get_customer_with_emails/${id}`).then(response => {
+    if (response.data.status == 200) {
+      customerEmails = response.data.customers
+    }
+  })
 
-    return customerEmails
+  return customerEmails
 }
 
 
 
 
 const getOrderIndexIds = async (id) => {
-    var orderIndexes = [];
+  var orderIndexes = [];
 
 
-    await axios.get(`get_order_index_ids_by_id/${id}`).then(response => {
-        if (response.data.status == 200) {
-            orderIndexes = response.data.orderIndexes
-        }
-    })
+  await axios.get(`get_order_index_ids_by_id/${id}`).then(response => {
+    if (response.data.status == 200) {
+      orderIndexes = response.data.orderIndexes
+    }
+  })
 
 
-    return orderIndexes
+  return orderIndexes
 }
 
 
