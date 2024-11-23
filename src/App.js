a3ebe81bec3fc27e4e2bf860edf2ad7d6bbe22f5
Abdul Malik <developer1@appletechlabs.com>
2024-11-23 10:32:58 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ axios.defaults.url = 'https://gateway.aahaas.com/api'
 // axios.defaults.baseURL = 'http://172.16.26.238:8000/api'
 // axios.defaults.data = 'http://172.16.26.238:8000'
 
-axios.defaults.baseURL = 'https://admin-api.aahaas.com/api'
-axios.defaults.data = 'https://admin-api.aahaas.com'
+axios.defaults.baseURL = 'https://staging-admin-api.aahaas.com/api'
+axios.defaults.data = 'https://staging-admin-api.aahaas.com'
 
 // const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
 // if (csrfTokenMeta) {
