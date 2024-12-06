c0a0d74f828f466215e1e73b37d83c0d4bb9b4a2
chamod2000 <maduwanthachamod01@gmail.com>
2024-12-06 21:18:05 +0530

    add version name

--- src/_nav.js
+++ src/_nav.js
@@ -27,7 +27,6 @@ function getNavigationHook(permissions){
       name: 'Dashboard',
       to: '/dashboard',
       icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-     
     },
     {
       component: CNavItem,
@@ -35,97 +34,36 @@ function getNavigationHook(permissions){
       to: '/orders',
       icon: <CIcon icon={cilCart} customClassName="nav-icon" />,
     },
-    // {
-    //   component: CNavItem,
-    //   name: 'Orders allocations',
-    //   to: '/allocation',
-    //   icon: <CIcon icon={cilGarage} customClassName="nav-icon" />,
-    // },
-    // {
-    //   component: CNavItem,
-    //   name: 'Orders calendar',
-    //   to: '/order-calendar',
-    //   icon: <CIcon icon={cilCart} customClassName="nav-icon" />,
-    // },
     {
       component: CNavTitle,
       name: 'Other',
     },
-    // {
-    //   component: CNavItem,
-    //   name: 'Sales',
-    //   to: '/sales',
-    //   icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-    // },
-  
-    // {
-    //   component: CNavItem,
-    //   name: 'Promotions & Offers',
-    //   to: '/offers_promo',
-    //   icon: <CIcon icon={cilTag} customClassName="nav-icon" />,
-    // },
-  
     {
       component: CNavItem,
       name: 'Vendors',
       to: '/vendors',
-      hidden:false,
+      hidden: false,
       icon: <CIcon icon={cilUserX} customClassName="nav-icon" />,
-      // items: [
-      //   {
-      //     component: CNavItem,
-      //     name: 'Vendors',
-      //     to: '/vendors/',
-      //   },
-      //   {
-      //     component: CNavItem,
-      //     name: 'Vendor Analytics',
-      //     to: 'vendors/analytics',
-      //   },
-  
-      // ],
     },
-  
-  
     {
       component: CNavItem,
       name: 'Customers',
       to: '/customers',
       icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
     },
-  
-  
-  
     {
       component: CNavItem,
       name: 'Sales Analytics',
       to: '/sales',
       icon: <CIcon icon={cilUserPlus} customClassName="nav-icon" />,
     },
-  
-  
     {
       component: CNavItem,
       name: 'Products',
       to: '/products',
       icon: <CIcon icon={cilGrain} customClassName="nav-icon" />,
-      // items: [
-      //   {
-      //     component: CNavItem,
-      //     name: 'Products',
-      //     to: '/products/',
-      //   },
-      //   // {
-      //   //   component: CNavItem,
-      //   //   name: 'Product Analytics',
-      //   //   to: 'products/analytics',
-      //   // },
-  
-      // ],
     },
-  
-  
-   {
+    {
       component: CNavGroup,
       name: 'Accounts',
       to: '/accounts',
@@ -144,18 +82,6 @@ function getNavigationHook(permissions){
         }
       ],
     },
-    // {
-    //   component: CNavItem,
-    //   name: 'Delivery',
-    //   to: '/delivery',
-    //   icon: <CIcon icon={cilCarAlt} customClassName="nav-icon" />,
-    // },
-    // {
-    //   component: CNavItem,
-    //   name: 'Testing',
-    //   to: '/theme/Testing',
-    //   icon: <CIcon icon={cilAddressBook} customClassName="nav-icon" />,
-    // },
     {
       component: CNavItem,
       name: 'Chat',
@@ -199,11 +125,11 @@ function getNavigationHook(permissions){
           name: 'Resend Emails',
           to: '/emails/all',
         },
-        // {
-        //   component: CNavItem,
-        //   name: 'Generate Emails',
-        //   to: '/emails/generate',
-        // }
+        {
+          component: CNavItem,
+          name: 'Generate Emails',
+          to: '/emails/generate',
+        }
       ],
     },
     {
