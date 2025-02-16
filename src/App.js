ec5e643b67bc0cf1c6efd28b1fa52a9bfa0e690a
chamod2000 <maduwanthachamod01@gmail.com>
2025-02-16 18:10:29 +0530

    fix: update image URL in App.js and change version to V1.1.28 in AppSidebar

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 //  axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 //
 
-axios.defaults.imageUrl = 'https://staging-gateway.aahaas.com/';
-// axios.defaults.imageUrl = 'https://gateway.aahaas.com/';
+// axios.defaults.imageUrl = 'https://staging-gateway.aahaas.com/';
+axios.defaults.imageUrl = 'https://gateway.aahaas.com/';
 
-axios.defaults.baseURL = 'https://staging-admin-api.aahaas.com/api';
-axios.defaults.data = 'https://staging-admin-api.aahaas.com';
+// axios.defaults.baseURL = 'https://staging-admin-api.aahaas.com/api';
+// axios.defaults.data = 'https://staging-admin-api.aahaas.com';
 
-axios.defaults.url = 'https://staging-gateway.aahaas.com/api'
-// axios.defaults.url = 'https://gateway.aahaas.com/api'
+// axios.defaults.url = 'https://staging-gateway.aahaas.com/api'
+axios.defaults.url = 'https://gateway.aahaas.com/api'
 
 // axios.defaults.baseURL = 'http://172.16.26.67:8000/api'
 // axios.defaults.data = 'http://172.16.26.67:8000'
@@ -38,8 +38,8 @@ axios.defaults.url = 'https://staging-gateway.aahaas.com/api'
 // axios.defaults.baseURL = 'http://172.16.26.121:8000/api'
 // axios.defaults.data = 'http://172.16.26.121:8000'
 
-// axios.defaults.baseURL = 'https://admin-api.aahaas.com/api'
-// axios.defaults.data = 'https://admin-api.aahaas.com'
+axios.defaults.baseURL = 'https://admin-api.aahaas.com/api'
+axios.defaults.data = 'https://admin-api.aahaas.com'
 
 
 // const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
