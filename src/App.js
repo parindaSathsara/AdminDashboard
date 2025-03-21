1a3883d07223beb09468a173682a815894483cc4
Hirusha11-cpu <developer6@appletechlabs.com>
2025-03-21 11:02:58 +0530

    Update axios default configurations for staging environment

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 //  axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 //
 
-// axios.defaults.imageUrl = 'https://staging-gateway.aahaas.com/';
-// axios.defaults.baseURL = 'https://staging-admin-api.aahaas.com/api';
-// axios.defaults.data = 'https://staging-admin-api.aahaas.com';
-// axios.defaults.url = 'https://staging-gateway.aahaas.com/api';
+axios.defaults.imageUrl = 'https://staging-gateway.aahaas.com/';
+axios.defaults.baseURL = 'https://staging-admin-api.aahaas.com/api';
+axios.defaults.data = 'https://staging-admin-api.aahaas.com';
+axios.defaults.url = 'https://staging-gateway.aahaas.com/api';
 
 // axios.defaults.imageUrl = 'https://gateway.aahaas.com/';
 // axios.defaults.baseURL = 'https://admin-api.aahaas.com/api'
@@ -30,8 +30,8 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 
 // axios.defaults.baseURL = 'http://172.16.26.67:8000/api'
 // axios.defaults.data = 'http://172.16.26.67:8000'
-axios.defaults.baseURL = 'http://192.16.26.61:8000/api'
-axios.defaults.data = 'http://192.16.26.61:8000'
+// axios.defaults.baseURL = 'http://192.16.26.61:8000/api'
+// axios.defaults.data = 'http://192.16.26.61:8000'
 
 // axios.defaults.baseURL = 'http://172.16.26.238:8000/api'
 // axios.defaults.data = 'http://172.16.26.238:8000'
