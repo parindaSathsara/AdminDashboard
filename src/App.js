d858e3ee6fb7ddb9fb7aacc6d1b068f4bebade40
Hirusha11-cpu <developer6@appletechlabs.com>
2025-06-24 15:23:48 +0530

    feat: add Forget Password page and update routing in App and routes files

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ const loading = (
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 const Login = React.lazy(() => import('./views/pages/login/Login'));
 const Register = React.lazy(() => import('./views/pages/register/Register'));
+const ForgetPassword = React.lazy(() => import('./views/pages/forgetpassword/ForgetPassword'));
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 const Dashboard = React.lazy(() => import('./views/dashboard/Orders'));
@@ -529,6 +530,7 @@ const addNotification1 = (newChat, chatId) => {
                     <Route exact path="/" name="Login Page" element={<Login />} errorElement={<Page404></Page404>} />
                     <Route exact path="/login" name="Login Page" element={<Login />} errorElement={<Page404></Page404>} />
                     <Route exact path="/register" name="Register Page" element={<Register />} errorElement={<Page404></Page404>} />
+                    <Route exact path="/forgetpassword" name="Forget Password Page" element={<ForgetPassword />} errorElement={<Page404></Page404>} />
                     <Route exact path="*" name="404" element={<Page404 />} errorElement={<Page404></Page404>} />
                   </>
                   :
