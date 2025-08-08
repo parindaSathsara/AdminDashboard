da4a143e0ef862577e7f190429ebe3c33f891fc0
Reetha04 <144661587+Reetha04@users.noreply.github.com>
2025-08-08 11:26:38 +0530

    Whatsapp_frontend

--- src/_nav.js
+++ src/_nav.js
@@ -175,12 +175,25 @@ function getNavigationHook(permissions) {
       to: '/sales',
       icon: <CIcon icon={cilChartLine} customClassName="nav-icon" />,
     },
+{
+  component: CNavGroup,
+  name: 'Promotions',
+  icon: <CIcon icon={cilBullhorn} customClassName="nav-icon" />,
+  items: [
     {
       component: CNavItem,
-      name: 'Promotions',
-      to: '/promotions',
-      icon: <CIcon icon={cilBullhorn} customClassName="nav-icon" />,
+      name: 'Push Notifications',
+      to: '/promotions',  // Changed to /promotions
+      // icon: <CIcon icon={cilBellExclamation} customClassName="nav-icon" />,
     },
+    {
+      component: CNavItem,
+      name: 'WhatsApp Notifications',
+      to: '/whatsapp-notifications',
+      // icon: <CIcon icon={cilChatBubble} customClassName="nav-icon" />,
+    }
+  ]
+},
     {
       component: CNavItem,
       name: 'Products',
